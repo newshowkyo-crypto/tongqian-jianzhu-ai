689543aef8fa813ced8fe2ebffcedd58bbbf0147
Xiaoyong <1701187961@qq.com>
2026-05-25 10:54:39 +0800

    feat(m29): safety + technical briefing auto generator with qrcode signoff

--- apps/api/src/modules/project-site/project-site.module.ts
+++ apps/api/src/modules/project-site/project-site.module.ts
@@ -3,6 +3,7 @@ import { Module } from '@nestjs/common';
 import { ApprovalModule } from '../approval/approval.module.js';
 
 import { ProjectSiteController } from './project-site.controller.js';
+import { BriefingService } from './briefing.service.js';
 import { ProjectSiteService } from './project-site.service.js';
 import { ScheduleService } from './schedule.service.js';
 
@@ -10,7 +11,7 @@ import { ScheduleService } from './schedule.service.js';
   controllers: [ProjectSiteController],
   exports: [ProjectSiteService],
   imports: [ApprovalModule],
-  providers: [ProjectSiteService, ScheduleService],
+  providers: [BriefingService, ProjectSiteService, ScheduleService],
 })
 // Nest modules are marker classes discovered through decorators.
 // eslint-disable-next-line @typescript-eslint/no-extraneous-class
